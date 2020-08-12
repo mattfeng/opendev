50c6b84b7168982315dae491a1e3c31e5e784641
mattfeng <mattfeng@mit.edu>
2020-08-12 00:00:54 -0400

    create cost table

--- gatsby-book/src/components/ec2pricing.js
+++ gatsby-book/src/components/ec2pricing.js
@@ -3,9 +3,44 @@ import pricingData from "../data/ec2-linux-demand.json"
 
 class EC2Pricing extends Component {
   render() {
-    return <div>
-      <p>{ JSON.stringify(pricingData) }</p>
-    </div>
+    let prices = pricingData["prices"]
+      .sort((a, b) => {
+        let delta = ('' + a["attributes"]["aws:ec2:instanceType"].split(".")[0]).localeCompare(b["attributes"]["aws:ec2:instanceType"].split(".")[0])
+
+        if (delta !== 0) {
+          return delta
+        }
+
+        return Number.parseInt(a["attributes"]["aws:ec2:vcpu"]) > Number.parseInt(b["attributes"]["aws:ec2:vcpu"])
+      })
+
+    return (
+      <table>
+        <tr>
+          <th>Instance Type</th>
+          <th>vCPU</th>
+          <th>Memory</th>
+          <th>Price</th>
+        </tr>
+
+        {
+          prices.map((i) => {
+            const attr = i["attributes"]
+            const price = Number.parseFloat(i["price"]["USD"]).toFixed(2)
+
+            return (
+              <tr>
+                <td>{ attr["aws:ec2:instanceType"] }</td>
+                <td>{ attr["aws:ec2:vcpu"] }</td>
+                <td>{ attr["aws:ec2:memory"] }</td>
+                <td>{ price }</td>
+              </tr>
+            )
+          })
+        }
+
+      </table>
+    )
   }
 }
 
